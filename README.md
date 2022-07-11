bacf8e06dffc6ca102bf255d185f69e3ab08d051
刘永杰 <13809696125@163.com>
2022-07-11 13:47:48 +0800

    init

---
+++ README.md
@@ -0,0 +1,50 @@
+####  vite-plugin-uniapp-to-group
+
+`vite-plugin-uniapp-to-group` 是 `uni-app` 开发字节小程序的时候，使用抖音团购模板的 `vite` 转换插件
+
+
+```javascript
+  npm i vite-plugin-uniapp-to-group -D
+  or
+  yarn add vite-plugin-uniapp-to-group -D
+```
+
+```javascript
+  // vite.config.ts
+  import UniappToGroup from 'vite-plugin-uniapp-to-group'
+
+  export default defineConfig({
+    plugins: [
+      ..., // 你的其他vite插件
+      UniappToGroup({
+        // 对应 package.json 中引入插件步骤
+        package: {
+          ttPlugins: {
+            dependencies: {
+              'microapp-trade-plugin': {
+                version: '1.0.5',
+                isDynamic: true
+              }
+            }
+          }
+        },
+        /**
+         * app 中可以是任何字段，插件会将字段和app.json对比，有就合并字段，没有就添加字段
+         * 合并字段 目前只处理 Array 和 Object 类型(注：[]和{}才会合并， null 类型不会合并)
+         * Object 类型字段合并，在key相同的情况会覆盖value
+         * Array 类型合并 是将两个数组合并
+        */
+        app: {
+          pages: [
+            // 下单页
+            'ext://microapp-trade-plugin/order-confirm',
+            // 退款申请页
+            'ext://microapp-trade-plugin/refund-apply',
+            // 退款详情页
+            'ext://microapp-trade-plugin/refund-detail'
+          ]
+        }
+      })
+    ]
+  })
+```
---
+++ src/index.ts
@@ -0,0 +1,94 @@
+import type { Plugin } from 'vite'
+
+export interface APP {
+  pages: string[]
+  [key: string]: any
+}
+
+export interface PACKAGE {
+  ttPlugins: {
+    dependencies: {
+      [key: string]: any
+    }
+    [key: string]: any
+  }
+  [key: string]: any
+}
+
+export interface TYPE_OPTIONS {
+  package: PACKAGE
+  app: APP
+}
+
+export default function UniappToGroup (options: TYPE_OPTIONS): Plugin {
+	let packages: PACKAGE = {
+		ttPlugins: {
+			dependencies: {}
+		}
+	}
+	let app: APP = {
+  	pages: []
+	}
+	if (options.package) {
+		packages = options.package
+	}
+	if (options.app) {
+		app = options.app
+	}
+
+	return {
+		name: 'vite-plugin-uniapp-to-group',
+		enforce: 'post',
+		generateBundle (code: any, bundle: any) {
+			let packageFlag = true
+			for (const name of Object.keys(bundle)) {
+				if (!Object.keys(app).length && !Object.keys(packages).length) {
+					break
+				}
+				if (name === 'app.json') {
+					const appJson = JSON.parse(bundle[name].source)
+					// if (app.pages) {
+					// 	appJson.pages = unique(appJson.pages.concat(app.pages))
+					// }
+					for (const key in app) {
+						if (appJson[key]) {
+							if (
+								Object.prototype.toString.call(appJson[key]) === '[object Object]' &&
+                Object.prototype.toString.call(app[key]) === '[object Object]'
+							) {
+								appJson[key] = {
+									...appJson[key],
+									...app[key]
+								}
+							}
+							if (
+								Array.isArray(appJson[key]) &&
+                Array.isArray(app[key])
+							) {
+								appJson[key] = appJson.pages.concat(app[key])
+							}
+						} else {
+							appJson[key] = app[key]
+						}
+					}
+
+					bundle[name].source = JSON.stringify(appJson)
+				}
+				if (name === 'package.json') {
+					packageFlag = false
+					let packageJson = JSON.parse(bundle[name].source)
+					packageJson = Object.assign(packageJson, packages)
+					bundle[name].source = JSON.stringify(packageJson)
+				}
+			}
+			if (Object.keys(packages).length && packageFlag) {
+				bundle['package.json'] = {
+					source: JSON.stringify(packages),
+					fileName: 'package.json',
+					type: 'asset',
+					name: undefined
+				}
+			}
+		}
+	}
+}
